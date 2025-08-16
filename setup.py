9ec9b6cff2d9d000cf884a9e5bbb3899e925d743
Sharjeel <sharjeel6392@gmail.com>
2025-08-16 12:54:30 +0530

    Project structure set. venv configured and ready to use

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Sharjeel',
+    author_email='sharjeel6392@gmail.com',
+    packages=find_packages(),
+)
